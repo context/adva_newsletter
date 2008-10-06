8345cb661c855be8cc08fcfb95a8337c69a05610
aurels <aurels@gangsta.local>
2008-10-06 21:39:30 +0200

    misc

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,10 +1,5 @@
 class Newsletter < ActiveRecord::Base
-  #acts_as_role_context
-
-  #delegate :topics_per_page, :comments_per_page, :to => :section
-
   belongs_to :site
-  belongs_to :section
 
   has_many :newsletter_messages, :order => "created_at DESC",
                                  :dependent => :delete_all
@@ -15,10 +10,4 @@ class Newsletter < ActiveRecord::Base
   validates_presence_of :from_email
   validates_format_of   :from_email,
                         :with => /(\A(\s*)\Z)|(\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z)/i
-
-  protected
-
-    def owner
-      section
-    end
 end
--- init.rb
+++ init.rb
@@ -1,2 +1,6 @@
 # remove plugin from load_once_paths 
 Dependencies.load_once_paths -= Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
+
+#Site.class_eval do
+#  has_many :newsletters
+#end
